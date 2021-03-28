872cab67557e909f284407217ef8f6eb64e48000
chenzhizs <chenzhi@qq.com>
2021-03-28 21:03:00 +0900

    在app开启画面，相当于app的封面上试用

--- PdUICourse/ViewController.swift
+++ PdUICourse/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ViewController viewDidLoad")
         // Do any additional setup after loading the view.
+        
+        label1.text = "label1.text"
     }
 
 
