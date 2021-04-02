10f7f55441c27092d390263f56c626738b4d57f3
chenzhizs <chenzhi@qq.com>
2021-04-02 10:53:36 +0900

    https://www.youtube.com/watch?v=ZJQ_4Q-JFIg&list=PL2SrkGHjnWcy_cnP6Ty8ItX0bTCTs1Ugx&index=19

--- PdUICourse/ViewController.swift
+++ PdUICourse/ViewController.swift
@@ -9,24 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    var intNumber = 0
     
-    @IBOutlet weak var btn1: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ViewController viewDidLoad")
         // Do any additional setup after loading the view.
         
-        label1.text = "请结账"
-        btn1.setTitle("结账", for: .normal)
+        counterLabel.text = "\(intNumber)";
+        
     }
     
-    
-    @IBAction func btn1Clicked(_ sender: UIButton) {
-        print("btn1Clicked")
-        btn1.setTitle("结账成功", for: .disabled)
+    @IBAction func plusOneByButton(_ sender: Any) {
+        intNumber += 1
+        counterLabel.text = "\(intNumber)";
         
-        btn1.isEnabled = false
+        
     }
 }
 
